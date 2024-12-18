4fc68cb0b59c2918bb105bf61158ae39ca2c7165
Wosk <krynio340@gmail.com>
2024-12-18 21:05:25 +0100

    Responsive design of login and singup forms

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -5,21 +5,25 @@ import { Link } from '@/src/i18n/routing';
 export default function Page() {
   const t = useTranslations('login');
   return (
-    <div className="flex h-full w-full flex-col gap-6 px-12 py-8 md:w-6/12 md:justify-center md:bg-slate-800">
-      <h1 className="text-5xl font-bold md:text-center">CargoLink</h1>
-      <p className="md:text-slate-400">
-        Dołącz do nas i uprość swoje procesy logistyczne. CargoLink – Twój niezawodny partner w
-        świecie transportu!
-      </p>
-      <LoginForm />
-      <p className="text-center">{t('or')}</p>
-      <Link
-        href="/auth/signup"
-        className="rounded-md border border-yellow-300 bg-slate-800 px-2 py-2 text-center"
-      >
-        <span className="text-white">{t('dontHaveAccount')}</span>{' '}
-        <span className="text-yellow-300">{t('register')}</span>
-      </Link>
+    <div className="flex h-full w-full flex-col justify-between px-4 py-4 md:w-6/12 md:justify-center md:gap-12 md:bg-slate-800 md:px-8">
+      <div className="flex flex-col gap-12">
+        <h1 className="text-5xl font-bold md:text-7xl">CargoLink</h1>
+        <p className="text-xl md:text-slate-400">
+          Dołącz do nas i uprość swoje procesy logistyczne. CargoLink – Twój niezawodny partner w
+          świecie transportu!
+        </p>
+      </div>
+      <div className={'w-full rounded-2xl bg-slate-800 p-6 md:p-0'}>
+        <LoginForm />
+        <p className="hidden text-center md:block">{t('or')}</p>
+        <Link
+          href="/auth/signup"
+          className="mt-2 block rounded-md border border-yellow-300 bg-slate-800 px-2 py-2 text-center"
+        >
+          <span className="text-white">{t('dontHaveAccount')}</span>{' '}
+          <span className="text-yellow-300">{t('register')}</span>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/app/ui/auth/login-form.tsx
+++ src/app/ui/auth/login-form.tsx
@@ -8,10 +8,10 @@ export default function LoginForm() {
   const t = useTranslations('login');
 
   return (
-    <form className="text-black">
-      <input type="email" name="email" placeholder="Email" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <Button>{t('login')}</Button>
+    <form className="flex flex-col gap-4 text-black">
+      <Input type="email" name="email" title="Email" required />
+      <Input type="password" name="password" title="Password" required />
+      <Button className="mt-4">{t('login')}</Button>
     </form>
   );
 }
