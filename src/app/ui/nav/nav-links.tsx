c5e8a3a71dc2287706f8af255ab4f3d10ca46103
Wosk <krynio340@gmail.com>
2024-12-22 11:50:55 +0100

    Fix for hide menu on phone

--- src/app/ui/nav/nav-links.tsx
+++ src/app/ui/nav/nav-links.tsx
@@ -5,17 +5,17 @@ import { useTranslations } from 'next-intl';
 
 type NavLinksProps = {
   links: Array<{ name: string; href: string }>;
-  handleMenuOpen: () => void;
+  handleClickLink: () => void;
 };
 
-export default function NavLinks({ links, handleMenuOpen }: NavLinksProps) {
+export default function NavLinks({ links, handleClickLink }: NavLinksProps) {
   const t = useTranslations('nav');
 
   return (
     <>
       {links.map((link) => {
         return (
-          <Link key={link.name} href={link.href} onClick={handleMenuOpen}>
+          <Link key={link.name} href={link.href} onClick={handleClickLink}>
             <p className="my-4 rounded-md bg-slate-700 px-2 py-2 text-center">{link.name}</p>
           </Link>
         );
