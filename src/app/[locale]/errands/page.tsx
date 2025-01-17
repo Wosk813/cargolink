50c30d985882f138b746df66d1bb8e764084d2cc
Krystian Tomczyk <krynio340@gmail.com>
2025-01-17 19:00:47 +0100

    Fix

--- src/app/[locale]/errands/page.tsx
+++ src/app/[locale]/errands/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="hidden text-3xl md:block">{t('errands')}</h1>
-      <Errands />
+      <Errands asModerator={false} />
     </div>
   );
 }
