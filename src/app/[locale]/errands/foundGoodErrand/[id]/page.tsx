d61fb4232bf4180262dd09cfd515ffcf0ddd8e98
Krystian Tomczyk <krynio340@gmail.com>
2025-01-18 14:03:11 +0100

    Enhance styling for found errand page: update layout and link appearance

--- src/app/[locale]/errands/foundGoodErrand/[id]/page.tsx
+++ src/app/[locale]/errands/foundGoodErrand/[id]/page.tsx
@@ -3,9 +3,11 @@ import { Link } from '@/src/i18n/routing';
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const errandId = (await params).id;
   return (
-    <div>
-      <p>Znaleziono zlecenie pasujące do ogłoszenia o planowanej trasie, które chcesz dodać</p>
-      <Link href={`/errands/${errandId}`}>Przejdź do zlecenia</Link>
+    <div className='flex flex-col gap-4'>
+      <p className='text-slate-400'>Znaleziono zlecenie pasujące do ogłoszenia o planowanej trasie, które chcesz dodać</p>
+      <Link className="rounded-md bg-yellow-300 p-2 text-black w-fit" href={`/errands/${errandId}`}>
+        Przejdź do zlecenia
+      </Link>
     </div>
   );
 }
