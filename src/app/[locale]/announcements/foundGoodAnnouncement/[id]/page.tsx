2903591e6440ff454833d04b882b9ac5536caa51
Krystian Tomczyk <krynio340@gmail.com>
2025-01-18 14:08:58 +0100

    Add translations and update UI for found announcements and errands

--- src/app/[locale]/announcements/foundGoodAnnouncement/[id]/page.tsx
+++ src/app/[locale]/announcements/foundGoodAnnouncement/[id]/page.tsx
@@ -1,11 +1,18 @@
-import { Link } from "@/src/i18n/routing";
+import { Link } from '@/src/i18n/routing';
+import { getTranslations } from 'next-intl/server';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const annoucementId = (await params).id;
+  const t = await getTranslations('addPost');
   return (
-    <div>
-      <p>Znaleziono ogłoszenie o planowanej trasie pasujące do zlecenia, które chcesz dodać</p>
-      <Link href={`/announcements/${annoucementId}`}>Przejdź do ogłoszenia</Link>
+    <div className="flex flex-col gap-4">
+      <p className="text-slate-400">{t('foundGoodAnnouncement')}</p>
+      <Link
+        className="w-fit rounded-md bg-yellow-300 p-2 text-black"
+        href={`/announcements/${annoucementId}`}
+      >
+        {t('goToAnnouncement')}
+      </Link>
     </div>
   );
 }
--- src/app/[locale]/errands/foundGoodErrand/[id]/page.tsx
+++ src/app/[locale]/errands/foundGoodErrand/[id]/page.tsx
@@ -1,12 +1,14 @@
 import { Link } from '@/src/i18n/routing';
+import { getTranslations } from 'next-intl/server';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const errandId = (await params).id;
+  const t = await getTranslations('addPost')
   return (
     <div className='flex flex-col gap-4'>
-      <p className='text-slate-400'>Znaleziono zlecenie pasujące do ogłoszenia o planowanej trasie, które chcesz dodać</p>
+      <p className='text-slate-400'>{t("foundGoodErrand")}</p>
       <Link className="rounded-md bg-yellow-300 p-2 text-black w-fit" href={`/errands/${errandId}`}>
-        Przejdź do zlecenia
+        {t("goToErrand")}
       </Link>
     </div>
   );
