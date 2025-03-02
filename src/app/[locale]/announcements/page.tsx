fd0da9fb813afd95f94e1f6875ca066b8591ade3
Wosk <krynio340@gmail.com>
2025-03-02 21:35:45 +0100

    Some test change

--- src/app/[locale]/announcements/page.tsx
+++ src/app/[locale]/announcements/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   const t = useTranslations('nav');
   return (
     <div className="flex flex-col gap-4">
-      <h1 className="hidden text-3xl md:block">{t('announcements')}</h1>
+      <h1 className="hidden text-3xl md:block">{t('announcements')}!</h1>
       <Annoucments asModerator={false} />
     </div>
   );
